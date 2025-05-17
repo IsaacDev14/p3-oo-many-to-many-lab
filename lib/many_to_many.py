6080f3ac88f93954d7ca8d6b75d27784516892eb
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-17 09:43:31 +0300

    Add Contract class with validated attributes and class-level tracking

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -34,8 +34,52 @@ class Book:
 
 
 class Contract:
+    all = []
+
     def __init__(self, author, book, date, royalties):
         self.author = author
         self.book = book
         self.date = date
         self.royalties = royalties
+        self.__class__.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception("author must be an instance of Author")
+        self._author = value
+
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, value):
+        if not isinstance(value, Book):
+            raise Exception("book must be an instance of Book")
+        self._book = value
+
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise Exception("date must be a string")
+        self._date = value
+
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, value):
+        if not isinstance(value, int):
+            raise Exception("royalties must be an integer")
+        self._royalties = value
+        
