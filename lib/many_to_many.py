4e92c90557671f70705c7994141b1a88cef74b78
IsaacDev14 <mwitiisaac14@gmail.com>
2025-05-17 09:13:12 +0300

    Add Book class with title attribute and class-level tracking

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,28 @@
 class Author:
-    pass
+    def __init__(self, name  ):
+        self.name = name
 
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title  ):
+        self.title = title
+        self.__class__.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise Exception("Title must be a string")
+        self._title = value
 
 
 class Contract:
-    pass
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
